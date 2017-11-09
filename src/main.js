4c637ee7d2ab96f8ed995844299fae4bf12761b6
Sergio Garcia <srgarcia91@gmail.com>
2017-11-09 12:37:00 +0100

    Directive for trigger the expression when the element is not clicked

--- src/main.js
+++ src/main.js
@@ -5,6 +5,24 @@ import App from './App';
 
 Vue.config.productionTip = false;
 
+Vue.directive('click-outside', {
+  bind: function bindOutsideClick(el, binding, vnode) {
+    const event = Object.prototype.hasOwnProperty.call(el, 'event')
+      ? el.event
+      : function hasClickedOutside(e) {
+        // here I check that click was outside the el and his childrens
+        if (!(el === event.target || el.contains(e.target))) {
+          // and if it did, call method provided in attribute value
+          vnode.context[binding.expression](e);
+        }
+      };
+    document.addEventListener('click', event);
+  },
+  unbind: function unbindOutsideClick(el) {
+    document.body.removeEventListener('click', el.event);
+  },
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
